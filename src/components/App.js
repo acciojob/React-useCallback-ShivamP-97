961d64f5e4d104620a3e2ce1d649be1b919c21a1
= <shivampathak19972@gmail.com>
2026-02-21 23:02:25 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
 
+import "../styles/App.css";
+import { UseCallbackComp } from "./UseCallbackComp";
+
+const App = () => {
+  return <UseCallbackComp />;
+};
+
+export default App;
